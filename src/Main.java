957f228ca218884f8cb766b30c520dd80a287ede
senko88 <kostyanchik88@gmail.com>
2020-02-12 21:12:32 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
+    public static final int TARGET_AGE = 18;
 
     public static void main(String[] args) {
-	// write your code here
+        List<MembersGroup> membersGroups = Arrays.asList(
+                new MembersGroup("group 1", Arrays.asList(
+                        new Member("Max", 25),
+                        new Member("Liza", 34),
+                        new Member("Alex", 29),
+                        new Member("Tom", 14),
+                        new Member("Linda", 43)
+                )),
+                new MembersGroup("group 2", Arrays.asList(
+                        new Member("Tina", 15),
+                        new Member("Mike", 13),
+                        new Member("Elon", 16),
+                        new Member("Bert", 17)
+                )),
+                new MembersGroup("group 3", Arrays.asList(
+                        new Member("Tina", 19),
+                        new Member("Mike", 21),
+                        new Member("Elon", 32),
+                        new Member("Bert", 47)
+                )),
+                new MembersGroup("group 4", Arrays.asList(
+                        new Member("Tina", 18),
+                        new Member("Mike", 18),
+                        new Member("Elon", 18),
+                        new Member("Bert", 18)
+                ))
+        );
+
+        Set<String> oldMembers = Finder.findOldMembers(membersGroups, TARGET_AGE);
+        List<String> groups = Finder.findGroupsWIthOldMembers(membersGroups, TARGET_AGE);
+
+        System.out.println(oldMembers);     // вывод людей по старому методу
+        System.out.println(groups);         // вывод групп людей
     }
 }
